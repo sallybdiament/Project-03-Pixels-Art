4e4642a5fbfc0c1183a366daff8920ae9d52dc3d
Sally Diament <sally.barzilai@gmail.com>
2022-04-20 23:29:26 -0300

    incluindo requisito 9

--- script.js
+++ script.js
@@ -14,3 +14,65 @@ function criaTabela () {
 }
 
 criaTabela();
+
+const primeiraCor = document.getElementsByClassName('color')[0];
+primeiraCor.classList += ' selected';
+// console.log(primeiraCor);
+
+const selectedElement = document.querySelector('.selected');
+// console.log(selectedElement);
+
+const firstLi = document.getElementsByClassName('color')[0];
+const secondLi = document.getElementsByClassName('color')[1];
+const thirdLi = document.getElementsByClassName('color')[2];
+const fourthLi = document.getElementsByClassName('color')[3];
+
+window.onload = function () {
+  firstLi.addEventListener('click', function () {
+  secondLi.classList.remove('selected');
+  thirdLi.classList.remove('selected');
+  fourthLi.classList.remove('selected');
+  firstLi.classList.add('selected');
+  });
+
+  secondLi.addEventListener('click', function (){
+secondLi.classList.add('selected');
+firstLi.classList.remove('selected');
+thirdLi.classList.remove('selected');
+fourthLi.classList.remove('selected');
+  })
+
+  thirdLi.addEventListener('click', function () {
+    firstLi.classList.remove('selected');
+    secondLi.classList.remove('selected');
+    fourthLi.classList.remove('selected');
+    thirdLi.classList.add('selected');
+    });
+    fourthLi.addEventListener('click', function () {
+        firstLi.classList.remove('selected');
+        secondLi.classList.remove('selected');
+        thirdLi.classList.remove('selected');
+        fourthLi.classList.add('selected');
+        });
+}
+
+const botaoLimpar = document.getElementById('clear-board');
+console.log(botaoLimpar);
+const selectAllPixels = document.querySelectorAll('div.pixel');
+
+botaoLimpar.addEventListener('click', function(){
+for (i = 0 ; i < 25 ; i +=1){
+selectAllPixels[i].style.backgroundColor = "white";
+}
+});
+
+//   function handleChangeSelected(event) {
+//     selectedElement.classList.remove('selected');
+//     event.target.classList.add('selected');
+    
+// }
+//   firstLi.addEventListener('click', handleChangeSelected(event));
+//   secondLi.addEventListener('click', handleChangeSelected(event));
+//   thirdLi.addEventListener('click', handleChangeSelected(event));
+//   fourthLi.addEventListener('click', handleChangeSelected(event));
+// }
