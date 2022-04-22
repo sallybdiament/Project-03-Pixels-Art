170b09ba4e8bf8f49da20d237bf7e6cd91395e25
Sally Diament <sally.barzilai@gmail.com>
2022-04-21 22:33:38 -0300

    incluindo o requisito 8

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const pixelBoard = document.getElementById('pixel-board');
 
-function criaTabela () {
+function criaTabela() {
   for (let index = 1; index < 6; index += 1) {
     const divLinhas = document.createElement('div');
     divLinhas.className = 'divLinhas';
@@ -27,59 +27,55 @@ const secondLi = document.getElementsByClassName('color')[1];
 const thirdLi = document.getElementsByClassName('color')[2];
 const fourthLi = document.getElementsByClassName('color')[3];
 
-window.onload = function () {
-  firstLi.addEventListener('click', function () {
+let colorToFill = '';
+
+firstLi.addEventListener('click', function () {
   secondLi.classList.remove('selected');
   thirdLi.classList.remove('selected');
   fourthLi.classList.remove('selected');
   firstLi.classList.add('selected');
-  });
+  colorToFill = 'black';
+});
 
-  secondLi.addEventListener('click', function (){
-secondLi.classList.add('selected');
-firstLi.classList.remove('selected');
-thirdLi.classList.remove('selected');
-fourthLi.classList.remove('selected');
-  })
+secondLi.addEventListener('click', function () {
+  secondLi.classList.add('selected');
+  firstLi.classList.remove('selected');
+  thirdLi.classList.remove('selected');
+  fourthLi.classList.remove('selected');
+  colorToFill = 'blue';
+})
 
-  thirdLi.addEventListener('click', function () {
-    firstLi.classList.remove('selected');
-    secondLi.classList.remove('selected');
-    fourthLi.classList.remove('selected');
-    thirdLi.classList.add('selected');
-    });
-    fourthLi.addEventListener('click', function () {
-        firstLi.classList.remove('selected');
-        secondLi.classList.remove('selected');
-        thirdLi.classList.remove('selected');
-        fourthLi.classList.add('selected');
-        });
-}
+thirdLi.addEventListener('click', function () {
+  firstLi.classList.remove('selected');
+  secondLi.classList.remove('selected');
+  fourthLi.classList.remove('selected');
+  thirdLi.classList.add('selected');
+  colorToFill = 'purple';
+});
+fourthLi.addEventListener('click', function () {
+  firstLi.classList.remove('selected');
+  secondLi.classList.remove('selected');
+  thirdLi.classList.remove('selected');
+  fourthLi.classList.add('selected');
+  colorToFill = 'red';
+  console.log(colorToFill);
+});
+
+
 
 const botaoLimpar = document.getElementById('clear-board');
 console.log(botaoLimpar);
 const selectAllPixels = document.querySelectorAll('div.pixel');
 
-botaoLimpar.addEventListener('click', function(){
-for (i = 0 ; i < 25 ; i +=1){
-selectAllPixels[i].style.backgroundColor = "white";
-}
+botaoLimpar.addEventListener('click', function () {
+  for (i = 0; i < 25; i += 1) {
+    selectAllPixels[i].style.backgroundColor = "white";
+  }
 });
 
-//   function handleChangeSelected(event) {
-//     selectedElement.classList.remove('selected');
-//     event.target.classList.add('selected');
-    
-// }
-//   firstLi.addEventListener('click', handleChangeSelected(event));
-//   secondLi.addEventListener('click', handleChangeSelected(event));
-//   thirdLi.addEventListener('click', handleChangeSelected(event));
-//   fourthLi.addEventListener('click', handleChangeSelected(event));
-// }
-
 document.addEventListener('click', function (event) {
-  if (event.target.classList.contains('pixel') ) {
-    event.target.style.backgroundColor = "black";
+  if (event.target.classList.contains('pixel')) {
+    event.target.style.backgroundColor = colorToFill;
     // event.target.classList.contains('pixel').style.backgroundColor = 'black';
   }
 }, false);
